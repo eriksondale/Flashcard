ed2fe13121187766c9236e308ad06921ed6d2acb
Dale Erikson <dje23@pitt.edu>
2018-09-15 14:49:45 -0400

    Building Deck and Card class...

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Determine appropriate data structure to use
 # Command-line arguments
 # GUI (later)
-# Want to add support for multiple decks at same time 
+# Want to add support for multiple decks at same time
 
 #Format
 # tab delimitted (front, back, know(0,1))
@@ -10,16 +10,38 @@
 
 class Card:
     # Card properties
+    def __init__(self, id, quest, ans):
+        self.id = id # ID variable, determined by deck class 
+        self.quest = quest # question variable
+        self.ans = ans # answer variable
+        self.knowIt = 0
+    def changeQuest(self, quest): #Change Values
+        self.quest = quest
+    def changeAns(self, ans):
+        self.ans = ans
+    def changeKnow(self, knowIt):
+        self.knowIt = knowIt
+    def getQuest(self): # Read Values
+        return self.quest
+    def getAns(self):
+        return self.ans
+    def getKnow(self):
+        return self.knowIt
+
+
 
 class Deck: # DS is TBD
-    def shuffle(self): # Shuffle deck
+    def __init__(self):
+        self.deck = [] # initialize empty list
+    def shuffle(self): # Shuffle deck, what's an efficient algorithm for this?
         pass
-    def add(): # Add Card to Deck
-        pass
+    def add(self, myCard): # Add Card to Deck
+        self.deck.append(myCard)
     def remove(): # Remove Card from Deck
         pass
-    def dump(): # Outputs all Deck Info
-        pass
+    def dump(self): # Outputs all Deck Info
+        for tempCard in self.deck:
+            print(tempCard.getQuest() + "\n" + tempCard.getAns() + "\n\n")
 
 
 print('Starting....')
@@ -38,5 +60,35 @@ while(True):
     print('0. Admin Info')
     usrIn = input('>')
     if(usrIn == 1): # Import Deck Function
+        # enter .fc (flash card) file
+        # try
+            # open file
+            # for line in file
+            # split based on \t
+            # construct card object
+            # add card object to Deck DS
+        # except
+            # print error reading file
+        pass
+    if(usrIn == 3):
+        myDeck = Deck()
+        tempQuest = raw_input("Enter one side of card: ")
+        tempAns = raw_input("Enter other side of card: ")
+        tempCard = Card(tempQuest, tempAns)
+        myDeck.add(tempCard)
+        print("Added Card!")
+    if(usrIn == 5): # Remove Card
+        # sort cards by ID
+        # List card ID and select to remove
+    if(usrIn == 6):
+        # shuffle()
+        # subloop and quiz
+        pass
     if(usrIn == 7):
+        # Make separate list with know==0 and shuffle()
+        # subloop and quiz
+        pass
+    if(usrIn == 8):
         break
+    if(usrIn == 0):
+        myDeck.dump()
